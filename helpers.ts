a6efe0ad2bc3777d76f2f719a1cb04e08311f5c0
Led <mehmetgoker1810@gmail.com>
2026-04-29 14:54:36 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T11:54:29.797Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-04-29T11:54:33.591Z
+Commit: Fix race condition in async operations
