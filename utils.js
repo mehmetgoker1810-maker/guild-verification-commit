6d9a8d8cd1394421e3dd7f85e5ce5f530a1d1337
Led <mehmetgoker1810@gmail.com>
2026-04-29 14:48:25 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T11:48:11.882Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-04-29T11:48:22.405Z
+Commit: Resolve timezone issues in date handling
