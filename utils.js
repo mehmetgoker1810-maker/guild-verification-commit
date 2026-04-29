879dcdbca4b935f224dbcbbca5edf30ddf1a4d06
Led <mehmetgoker1810@gmail.com>
2026-04-29 14:48:15 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T11:48:09.696Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-04-29T11:48:11.882Z
+Commit: Fix broken links in documentation
