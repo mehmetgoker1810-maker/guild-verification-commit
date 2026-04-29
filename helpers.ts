803f6db9919f5a2bac486a719d5230e76ac4dd6c
Led <mehmetgoker1810@gmail.com>
2026-04-29 14:54:17 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T11:52:08.378Z
-Commit: Implement user profile customization
+Timestamp: 2026-04-29T11:54:14.310Z
+Commit: Fix CSS styling bug on mobile devices
