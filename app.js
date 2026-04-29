491b502d6db893f43d1a1c2643891bc2f6f0a274
Led <mehmetgoker1810@gmail.com>
2026-04-29 14:56:21 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T11:56:06.290Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-04-29T11:56:18.991Z
+Commit: Fix broken links in documentation
