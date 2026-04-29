8cd87082573680dbe53f8cde91171d98d56aa538
Led <mehmetgoker1810@gmail.com>
2026-04-29 14:55:33 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T11:55:25.409Z
-Commit: Add multi-language support
+Timestamp: 2026-04-29T11:55:30.584Z
+Commit: Patch security vulnerability in authentication
