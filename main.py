0dd93564ef31b738e9d87529d17349d2df0b60f2
Led <mehmetgoker1810@gmail.com>
2026-04-29 14:49:28 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T11:49:15.615Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-04-29T11:49:26.199Z
+Commit: Fix memory leak in event handlers
