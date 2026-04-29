1b0f305bbdbb018860ff510886eb4435e3d59dc5
Led <mehmetgoker1810@gmail.com>
2026-04-29 14:57:39 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T11:56:18.991Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-04-29T11:57:36.813Z
+Commit: Resolve null pointer exception in user service
