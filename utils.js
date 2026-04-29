b5e32abbf1b9e332bc8b63af8d47b5729708e258
Led <mehmetgoker1810@gmail.com>
2026-04-29 14:54:26 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T11:54:10.664Z
-Commit: Implement user profile customization
+Timestamp: 2026-04-29T11:54:23.864Z
+Commit: Fix memory leak in event handlers
