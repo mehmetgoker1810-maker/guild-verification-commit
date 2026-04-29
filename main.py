02ab87bde00d25e6dc0de853b53780127c7559bb
Led <mehmetgoker1810@gmail.com>
2026-04-29 14:57:51 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T11:57:38.702Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-04-29T11:57:48.607Z
+Commit: Fix memory leak in event handlers
