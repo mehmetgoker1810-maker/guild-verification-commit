61d89eafc0ed5adc6981786c5128127a5e63b552
Led <mehmetgoker1810@gmail.com>
2026-04-29 14:48:12 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T11:45:18.552Z
-Commit: Fix database connection timeout
+Timestamp: 2026-04-29T11:48:09.696Z
+Commit: Fix race condition in async operations
